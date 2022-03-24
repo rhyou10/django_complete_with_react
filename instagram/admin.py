e86e72cc8663ccd2e5cfa59a984814a200d51a1d
rhyou10 <rhyou10@nate.com>
2022-03-24 22:32:30 +0900

    admin을 통한 데이터 관리

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+#admin.site.register(Post) #모델등록 첫번쨰 방법
+
+''' 모델등록 두번째 방법
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
+'''
+
+#모델등록 세번째 방법
+@admin.register(Post) #wrapping
+class PostAdmin(admin.ModelAdmin):
+    list_display =['pk', 'message', 'message_length', 'is_public' ,'created_at', 'updated_at'] #모델에서 정의된 필드들 함수들을 넣을수 있다. admin 페이지에서 확인가능
+    list_display_links = ['message'] #message를 통해 해당 Post로 들어갈수 있게한다(여러개 설정가능)
+
+    list_filter = ['created_at', 'is_public'] # admin/ instagrm Post 모델에 오른쪽에 filter를 확인가능(시간순으로)   / filter가 여러개인경우 and 조건으로 보여준다.
+
+    search_fields = ['message'] #admin page에서 Post 찾을때 검색을 message 기준으로
+
+    def message_length(self, post):
+        return len(post.message)
--- instagram/models.py
+++ instagram/models.py
@@ -3,7 +3,17 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=rue)
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
 
-# Create your models here.
+    # 자바 toString
+
+    def __str__(self): # admin에서 class -> list_display 설정하여 보이지 않는다
+        #return f"Custom Post objects ({self.id}) " 
+        return f"{self.pk} : {self.message}"
+
+    # def message_length(self):
+    #     return len(self.message)
+    # message_length.short_description = "메시지 글자수" ## admin에서 display_list 할경우 colunm 이름 변경
