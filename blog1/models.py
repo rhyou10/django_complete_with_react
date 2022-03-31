293463621f54f145928a4821e63c551cdba6b445
rhyou10 <rhyou10@nate.com>
2022-03-31 22:26:34 +0900

    migrate 다양한 기능 실습

--- blog1/models.py
+++ blog1/models.py
@@ -1,7 +1,11 @@
+from tkinter import CASCADE
 from django.db import models
+from django.conf import settings
 
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='+') #instagram post 와 똑같아서 related_name 포기시킴
+    #author는 나중에 추가한 필수필드임, null=blank=false 이기 떄문이다.
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
