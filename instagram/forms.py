729ab2fc39c7b72226eafd41c7096bb369a9c04e
rhyou10 <rhyou10@nate.com>
2022-04-13 22:36:26 +0900

    form validator

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Post
+import re
 
 #모델폼과 일반폼 차이점
 #일반 폼은 폼 자체에서 데이터 받는 검증 형식 지정해야한다.
@@ -11,5 +12,12 @@ class PostForm(forms.ModelForm):
         # fields = '__all__'
         fields = [
             'message', 'photo', 'tag_set', 'is_public'
-        ]
+        ] #이것만하면 문제점 여기서는 작성자를 지정하지않아서
+
+    def clean_message(self):# 유효성검사시 자동으로 실행된다. / clean_필드명으로 필드에 유효성검사
+        message = self.cleaned_data.get('message')
+        if message:
+            message = re.sub('[a-zA-z]', '', message)
+        
+        return message
 
