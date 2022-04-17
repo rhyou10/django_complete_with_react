7b3848172b49cf3f637a7c329c6ea80dd81d97dc
rhyou10 <rhyou10@nate.com>
2022-04-17 23:23:08 +0900

    로그인 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,9 @@
+from re import template
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
 urlpatterns = [
+    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login')
 
 
 ]
