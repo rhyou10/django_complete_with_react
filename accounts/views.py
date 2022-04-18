c0fe00249d0608cf7f9924a485c2207593a844c5
rhyou10 <rhyou10@nate.com>
2022-04-18 23:27:16 +0900

    user 프로필 페이지 생성 및 수

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from pipes import Template
+from django.shortcuts import redirect, render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required 
+from django.views.generic import TemplateView, UpdateView
+from .models import Profile
+from .forms import ProfileFrom
 
+# @login_required
+# def profile(request):
+#     return render(request, 'accounts/profile.html')
 # Create your views here.
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/profile.html'
+    
+profile = ProfileView.as_view()
+
+# class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+#     model = Profile
+#     form_class =  ProfileFrom
+
+# profile_edit =ProfileUpdateView.as_view()
+
+@login_required
+def profile_edit(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = None
+
+    if request.method =='POST':
+        form = ProfileFrom(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+    else:
+        form = ProfileFrom(instance=profile)
+    return render(request, 'accounts/profile_form.html',{
+        'form':form,
+    })
