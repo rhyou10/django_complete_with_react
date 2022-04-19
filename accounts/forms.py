b33fe41dcb2bcdc1b41ec6fa4fb025825752c5c0
rhyou10 <rhyou10@nate.com>
2022-04-19 22:43:40 +0900

    로그인시 질문 추가

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,20 @@
 from django import forms
 from .models import Profile
+from django.contrib.auth.forms import AuthenticationForm
 
 class ProfileFrom(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['address', 'zipcode']
+
+
+
+class LoginForm(AuthenticationForm):
+    answer = forms.IntegerField(help_text= '3+3=?')
+
+    def clean_answer(self):
+        answer = self.cleaned_data.get('answer')
+        if answer !=6:
+            raise forms.ValidationError('떙')
+        return answer
+    pass
